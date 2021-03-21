b24296b8df30dc43bb2e6b97e278f50e5ede21c7
natandias <49736127+natandias@users.noreply.github.com>
2021-03-21 20:56:42 -0300

    Config prettier for frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,62 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vit2e" />
-  <p>'ABC': {{repositories}}</p>
+    <Navbar />
 </template>
 
 <script lang="ts">
-
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 import { ref, onMounted } from 'vue'
+import Navbar from './components/layout/Navbar.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      repositories: []
-    }
-  },
-  async mounted() {
-
-     const response = await fetch(`http://localhost:5000/weatherforecast`,{
+    name: 'App',
+    components: { Navbar },
+    data() {
+        return {
+            repositories: [],
+        }
+    },
+    async mounted() {
+        const response = await fetch(`http://localhost:5000/weatherforecast`, {
             mode: 'no-cors',
             headers: {
-              'Access-Control-Allow-Origin':'*'
-            }
-          })
-          console.log("response", await response.json())
+                'Access-Control-Allow-Origin': '*',
+            },
+        })
+        console.log('response', await response.json())
 
-          this.repositories = await response.json();
-    }
-  // setup(props) {
-  //   console.log(props) // { user: '' }
+        this.repositories = await response.json()
+    },
+    // setup(props) {
+    //   console.log(props) // { user: '' }
 
-  //   const repositories = ref([])
-  //   const getUserRepositories = async () => {
-  //     repositories.value = await fetch(`${process.env.VUE_API_URL}/weatherforecast`)
-  //   }
+    //   const repositories = ref([])
+    //   const getUserRepositories = async () => {
+    //     repositories.value = await fetch(`${process.env.VUE_API_URL}/weatherforecast`)
+    //   }
 
-  //   onMounted(getUserRepositories)
+    //   onMounted(getUserRepositories)
 
-  //   return {
-  //     repositories,
-  //     getUserRepositories
-  //   }
-  // }
+    //   return {
+    //     repositories,
+    //     getUserRepositories
+    //   }
+    // }
 })
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/layout/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav
+        class="flex bg-gray-600 fixed w-full items-center justify-between px-6 h-16 bg-white text-gray-700 border-b border-gray-200 z-10"
+    >
+        <h1>abc</h1>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ref, onMounted } from 'vue'
+
+export default defineComponent({
+    name: 'Navbar',
+    data() {
+        return {}
+    },
+    mounted() {},
+})
+</script>
+
+<style>
+</style>
